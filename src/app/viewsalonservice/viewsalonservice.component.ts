8d8ca607141b3eee654deac88fb32f38055f62c6
SubhajitSaha7 <subhajit1cool@gmail.com>
2021-06-29 14:32:09 +0530

    design started

--- src/app/viewsalonservice/viewsalonservice.component.ts
+++ src/app/viewsalonservice/viewsalonservice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../storage.service';
 
 @Component({
   selector: 'app-viewsalonservice',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewsalonserviceComponent implements OnInit {
 
-  constructor() { }
+  constructor(public storageservice:StorageService) { }
 
   ngOnInit() {
   }
